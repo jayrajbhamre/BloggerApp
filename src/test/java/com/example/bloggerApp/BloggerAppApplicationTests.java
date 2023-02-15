22af26aa34b4c0c53e21be5f194520224b0d66ef
Jayrajbhamre <jayrajbhamre@gmail.com>
2023-02-15 10:57:32 +0530

    first commit

---
+++ src/test/java/com/example/bloggerApp/BloggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloggerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
